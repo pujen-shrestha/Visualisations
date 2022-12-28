6b8f4f662dbf45e9faf724de014d678c3d7a66a6
pujen-shrestha <pujen.pwl.shrestha@gmail.com>
2022-12-28 06:15:35 +0000

    Update to date

---
+++ Helper Functions.R
@@ -0,0 +1,24 @@
+
+# Pujen Standard Plotting Colors
+
+pujPaletteOlive1 <- "#624658"
+
+pujEbony1 <- "#647A5F"
+
+pujGreen1 <- "#559B54"
+
+pujYellow1 <- "#CFD186"
+
+pujOpal1 <- "#E3655B"
+  
+pujDarkGreen2 <- "#071E22"
+
+pujGreen2 <- "#1D7874"
+  
+pujeWinterGreen2 <- "#679289"
+  
+pujPeach2 <- "#F4C095"
+  
+pujRed2 <- "#EE2E31"
+
+pujGrey <- "#5B636A"
